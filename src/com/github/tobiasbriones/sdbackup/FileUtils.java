4c4ccef2278de8caca04553cc4ed8f4492dbf359
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-11-29 22:57:46 -0600

    Create class FileUtils

---
+++ src/com/github/tobiasbriones/sdbackup/FileUtils.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of SD Backup.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package com.github.tobiasbriones.sdbackup;
+
+public final class FileUtils {
+    private FileUtils() {}
+}

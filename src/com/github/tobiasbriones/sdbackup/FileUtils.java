34eee635a5fadc5e5b3fba32ca13a036ba352c4b
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-11-30 11:59:29 -0600

    Implement function copyDirectory

--- src/com/github/tobiasbriones/sdbackup/FileUtils.java
+++ src/com/github/tobiasbriones/sdbackup/FileUtils.java
@@ -12,6 +12,36 @@
 
 package com.github.tobiasbriones.sdbackup;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public final class FileUtils {
+    public static void copyDirectory(File src, File dst) throws IOException {
+        copyDirectory(src.toPath(), dst.toPath());
+    }
+
+    public static void copyDirectory(Path src, Path dst) throws IOException {
+        try (Stream<Path> walk = Files.walk(src)) {
+            final List<Path> paths = walk.collect(Collectors.toList());
+
+            for (Path path : paths) {
+                copyFile(src, path, dst);
+            }
+        }
+    }
+
+    private static void copyFile(Path rootSrc, Path src, Path rootDst) throws IOException {
+        final String srcStr = src.toString();
+        final String relDst = srcStr.substring(rootSrc.toString().length());
+        final Path dst = Paths.get(rootDst.toString(), relDst);
+        Files.copy(src, dst);
+    }
+
     private FileUtils() {}
 }

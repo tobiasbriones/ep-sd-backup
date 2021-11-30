6c862b9ab640fb407e1e96ba64c1defe1e3da94d
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-11-30 12:30:27 -0600

    Add package-info.java to project packages

---
+++ src/dev/tobiasbriones/sdbackup/ui/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of SD Backup.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+/**
+ * @author Tobias Briones
+ */
+package dev.tobiasbriones.sdbackup.ui;

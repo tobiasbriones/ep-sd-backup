eb83a15d8e05949b950919ab233fa44361ae5ba1
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-11-30 12:21:57 -0600

    Clean

--- src/com/github/tobiasbriones/sdbackup/ui/MWController.java
+++ src/com/github/tobiasbriones/sdbackup/ui/MWController.java
@@ -17,8 +17,6 @@ import com.github.tobiasbriones.sdbackup.model.BackupTask;
 
 import javax.swing.*;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
